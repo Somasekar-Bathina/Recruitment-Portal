df42afb18b8530faca7bebd72e0859cc1212ce20
Somasekar-Bathina <98614027+Somasekar-Bathina@users.noreply.github.com>
2022-04-17 12:55:27 -0500

    Project Skelton Push

---
+++ src/test/java/com/project/fdb/Recruitment/Portal/RecruitmentPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.fdb.Recruitment.Portal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecruitmentPortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
