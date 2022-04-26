971f521c72233105dafa544d5d9fe2d07dfe7ce5
Somasekar-Bathina <somasekarbathinasuresh@my.unt.edu>
2022-04-25 23:35:27 -0500

    Commenting push

--- src/main/java/com/project/fdb/Recruitment/Portal/Model/JobOfferDetails.java
+++ src/main/java/com/project/fdb/Recruitment/Portal/Model/JobOfferDetails.java
@@ -1,18 +1,18 @@
-package com.project.fdb.Recruitment.Portal.Model;
-
-import java.sql.Date;
-import java.sql.Timestamp;
-
-import javax.persistence.Column;
-import javax.persistence.EmbeddedId;
-import javax.persistence.Entity;
-import javax.persistence.Table;
-
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
+//package com.project.fdb.Recruitment.Portal.Model;
+//
+//import java.sql.Date;
+//import java.sql.Timestamp;
+//
+//import javax.persistence.Column;
+//import javax.persistence.EmbeddedId;
+//import javax.persistence.Entity;
+//import javax.persistence.Table;
+//
+//import lombok.AllArgsConstructor;
+//import lombok.Builder;
+//import lombok.Data;
+//import lombok.NoArgsConstructor;
+//
 //@Data
 //@Builder
 //@Entity
@@ -28,4 +28,4 @@ import lombok.NoArgsConstructor;
 //	private String jobcode_id;
 //	
 //	@Column(name =  "")
-}
+//}
